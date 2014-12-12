ca02c97c565f6142016b61abf1823046f0178145
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2014-12-12 12:55:59 +0300

    monkeypath fix

--- lib/activeadmin-poro-decorator.rb
+++ lib/activeadmin-poro-decorator.rb
@@ -7,7 +7,8 @@ module ActiveAdmin
   class Comment < ActiveRecord::Base
     # @returns [String] The name of the record to use for the polymorphic relationship
     def self.resource_type(resource)
-      resource.type
+      return resource.type if resource.respond_to?(:type)
+      ResourceController::Decorators.undecorate(resource).class.name.to_s
     end
   end
 end
