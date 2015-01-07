74eb698ad756a54ae294112d7b6bc5d2a55120bf
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2015-01-08 01:41:47 +0200

    test added

--- spec/integration/decorate_spec.rb
+++ spec/integration/decorate_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe 'Decorate' do
+  let(:article) { double('article') }
   it 'decorates properly' do
+    allow(article).to receive(:title).and_return('Aslak')
+    expect(ArticlePresenter.new(article).hello).to eq 'Hello, Aslak'
   end
 end
---
+++ spec/support/dummy/app/presenters/article_presenter.rb
@@ -0,0 +1,25 @@
+# inspired by https://github.com/activeadmin/activeadmin/blob/a180c76f17480715948bff2ead827f338d95a835/spec/support/templates/post_decorator.rb
+class ArticlePresenter < DelegateClass(Article)
+  include ActiveadminPoroDecorator
+
+  class << self
+    delegate :all, :arel_table, :find_by_sql, :columns, :connection,\
+             :unscoped, :table_name, :primary_key, to: Article
+
+    def model_name
+      ActiveModel::Name.new Article
+    end
+
+    def build_default_scope
+      Article.send(:build_default_scope)
+    end
+  end
+  
+  def hello
+    "Hello, #{title}"
+  end
+
+  def link_title
+    helpers.link_to(id, url_helpers.admin_article_path(self))
+  end
+end
